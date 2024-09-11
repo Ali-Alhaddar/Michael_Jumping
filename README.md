3ff586193f197610cfabcc4a6d399ac5f2f4d6db
Lushfr <alaoealhaddar@gmail.com>
2024-09-11 13:47:49 +0300

    Project complete

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Title
+michael jumping
+## Date: 3/8/2024
+
+# By: Ali Alhaddar
+hello i'm ali alhaddar and i'm a GA student
+
+[Linkedin](https://www.linkedin.com/in/ali-alhaddar-401354325)
+
+[GitHub](https://github.com/Lushfr)
+***
+
+
+### ***Description***
+#### for my first Project i will create a game called michael jumping that similer to dino game. in my edition i will replace the dino object with michael's spi
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const michael = document.getElementById('michael');
+    const gameArea = document.getElementById('gameArea');
+    let isGameOver = false;
+    let score = 0;
+    let startTime;
+    let scoreDisplay;
+    let hitSound = new Audio('sound/e37e-e2ef-4e58-a50b-0374edfd0e78.mp3'); // Add the path to your sound file here
+
+    function createScoreDisplay() {
+        scoreDisplay = document.createElement('div');
+        scoreDisplay.classList.add('score');
+        gameArea.appendChild(scoreDisplay);
+    }
+
+    let timer = () => {
+        score += 10;
+        scoreDisplay.innerText = score;
+    }
+
+    const setIntervalID = setInterval(timer, 1000);
+
+    function createDuck() {
+        if (!isGameOver) {
+            const duck = document.createElement('div');
+            duck.classList.add('duck');
+
+            duck.style.top = (gameArea.offsetHeight - 50) + 'px';
+            duck.style.right = '0';
+
+            gameArea.appendChild(duck);
+
+            moveDuck(duck);
+        }
+    }
+
+    function moveDuck(duck) {
+        let duckPosition = parseInt(duck.style.right, 10);
+
+        function frame() {
+            duckPosition += 5;
+            duck.style.right = duckPosition + 'px';
+
+            if (duckPosition > gameArea.offsetWidth) {
+                gameArea.removeChild(duck);
+                cancelAnimationFrame(duckAnimation);
+            } else {
+                if (checkCollision(michael, duck)) {
+                    endGame();
+                } else {
+                    duckAnimation = requestAnimationFrame(frame);
+                }
+            }
+        }
+
+        let duckAnimation = requestAnimationFrame(frame);
+    }
+
+    function checkCollision(michael, duck) {
+        let michaelRect = michael.getBoundingClientRect();
+        let duckRect = duck.getBoundingClientRect();
+
+        return !(
+            michaelRect.bottom < duckRect.top ||
+            michaelRect.top > duckRect.bottom ||
+            michaelRect.right < duckRect.left ||
+            michaelRect.left > duckRect.right
+        );
+    }
+
+    function endGame() {
+        isGameOver = true;
+        hitSound.play(); // Play the sound effect
+        alert('Game Over! You hit the duck.');
+        location.reload();
+    }
+
+    function jump() {
+        if (michael && !michael.classList.contains('jump')) {
+            michael.classList.add('jump');
+            setTimeout(() => {
+                michael.classList.remove('jump');
+            }, 700);
+        }
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.code === 'Space') {
+            jump();
+        }
+    });
+
+    setInterval(createDuck, 2300);
+    createScoreDisplay();
+});
